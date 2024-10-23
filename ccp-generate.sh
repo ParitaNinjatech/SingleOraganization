d97349237e00312291b87c540d2078c9ba213beb
ParitaNinjatech <parita@ninjatechnolabs.com>
2024-10-23 17:47:59 +0530

    first commit

--- ccp-generate.sh
+++ ccp-generate.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+# Function to base64 encode the PEM file
 function one_line_pem() {
     echo "=================  Generating Connection Profile  ================"
-    echo "$(awk 'NF {sub(/\\n/, ""); printf "%s\\\\\\\n",$0;}' $1)"
+    # Base64 encode the PEM content to avoid issues with special characters
+    base64 -w 0 $1
 }
 
+# Function to escape the base64 string for sed
+function escape_for_sed() {
+    echo $1 | sed -e 's/[\/&]/\\&/g'
+}
+
+# Function to generate JSON CCP using sed
 function json_ccp() {
     local PP=$(one_line_pem $5)
     local CP=$(one_line_pem $6)
+    PP=$(escape_for_sed "$PP")
+    CP=$(escape_for_sed "$CP")
+    echo "PEER PEM (escaped): $PP"
+    echo "CA PEM (escaped): $CP"
     sed -e "s/\${ORG}/$1/" \
         -e "s/\${P0PORT}/$2/" \
         -e "s/\${P1PORT}/$3/" \
@@ -17,9 +29,14 @@ function json_ccp() {
         templates/ccp-template.json
 }
 
+# Function to generate YAML CCP using sed
 function yaml_ccp() {
     local PP=$(one_line_pem $5)
     local CP=$(one_line_pem $6)
+    PP=$(escape_for_sed "$PP")
+    CP=$(escape_for_sed "$CP")
+    echo "PEER PEM (escaped): $PP"
+    echo "CA PEM (escaped): $CP"
     sed -e "s/\${ORG}/$1/" \
         -e "s/\${P0PORT}/$2/" \
         -e "s/\${P1PORT}/$3/" \
@@ -29,6 +46,7 @@ function yaml_ccp() {
         templates/ccp-template.yaml | sed -e $'s/\\\\n/\\\n        /g'
 }
 
+# Set variables for Org1
 ORG=1
 P0PORT=7051
 P1PORT=8051
@@ -36,5 +54,6 @@ CAPORT=7054
 PEERPEM=crypto-config/peerOrganizations/org1.bionic.com/tlsca/tlsca.org1.bionic.com-cert.pem
 CAPEM=crypto-config/peerOrganizations/org1.bionic.com/ca/ca.org1.bionic.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" >connection-org1.json
-echo "$(yaml_ccp $ORG $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" >connection-org1.yaml
+# Generate JSON and YAML connection profiles
+echo "$(json_ccp $ORG $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" > connection-org1.json
+echo "$(yaml_ccp $ORG $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" > connection-org1.yaml
